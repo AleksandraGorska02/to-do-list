0548248abbb01741d2b0b40d4da5f8c53cb24b0d
Aleksandra Gorska <aleksandra.gorska02@gmail.com>
2023-04-21 18:38:41 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck,faPen,faTrash } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [toDo, setToDo]=useState([
@@ -13,31 +14,44 @@ function App() {
   const [newTask, setNewTask]=useState('');
   const [updateData, setUpdate,Data]=useState('');
 
-  const addTask=()=>{
+  const addTask = () => {
+    if (newTask) {
+      const newTaskadd = { id: uuidv4(), title: newTask, status: false };
+      setToDo([...toDo, newTaskadd]);
+      setNewTask("");
+    }
+  };
+  
 
-//
-  }
+  
   const deleteTask=(id)=>{
-
-    //
+ 
+    const updatedToDo = toDo.filter((task) => task.id !== id);
+    setToDo(updatedToDo);
+    
   }
   const markDone=(id)=>{
-
-    //
-  }
-  const cancelUpdate=()=>{
-
-    //
-  }
+    let newTask = toDo.map(task=>{if(task.id===id){
+       if(task.status===true){
+     return({...task,status:false}) 
+    }
+    if(task.status ===false){
+      return({...task,status:true}) 
+    }
+   }
+      return task;
+    
+    }
+      )
+   setToDo(newTask);
+    
+}
+  
   const changeTask=(e)=>{
 
     //
   }
-  const updateTask=()=>{
-
-    //
-  }
-
+ 
 
 
 
@@ -52,6 +66,66 @@ function App() {
 
         {toDo && toDo.length ? '' : 'brak zadań'}
 
+        {toDo && toDo 
+
+          
+          .map( (task, index) => {
+                return(
+                  <React.Fragment key={task.id}>
+                    <div className='col taskBg'>
+
+                        <div className={task.status ? 'done' : ''}>
+
+                          <span className='taskNumber'>{index + 1}</span>
+                          <span className='taskText'>{task.title}</span>
+
+                        </div>
+
+                        <div className='iconWrap'>
+                          <span title='yes / no' onClick={(e)=>markDone(task.id)}>
+                            <FontAwesomeIcon icon={faCircleCheck}></FontAwesomeIcon>
+                          </span>
+                          <span title='edit'>
+                            <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+                          </span>
+                          <span title='delete' onClick={()=>deleteTask(task.id)}
+                          >
+                            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                          </span>
+                        </div>
+
+                    </div>
+                    
+                  </React.Fragment>
+                )
+          })
+        }
+
+        <div className='row'>
+          <div className='col'>
+            <input className='form-control form-control-lg'></input>
+          </div>
+            <div className='col-auto'>
+              <button className='btn btn-lg btn-success mr'>uaktualnij</button>
+              <button className='btn btn-lg btn-warning'>anuluj</button>
+              
+            </div>
+        </div>
+        <br/>
+        <div className='row'>
+          <div className='col'>
+            <input 
+            value={newTask}
+            onChange={(e)=>setNewTask(e.target.value)}
+            className='form-control form-control-lg'></input>
+          </div>
+            <div className='col-auto'>
+              <button
+              onClick={addTask}
+               className='btn btn-lg btn-success mr'>dodaj</button>
+                           
+            </div>
+        </div>
     </div>
   );
 }
